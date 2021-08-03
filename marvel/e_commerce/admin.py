479519f27c19114f090f68c6052192e7c79d2a2e
marcosluduea89 <72500833+marcosluduea89@users.noreply.github.com>
2021-08-03 13:27:08 -0300

    commit 1

--- marvel/e_commerce/admin.py
+++ marvel/e_commerce/admin.py
@@ -1,34 +1,59 @@
 from django.contrib import admin
 
 # NOTE: Tenemos que importar los modelos con los que vamos a trabajar:
-# from e_commerce.models import *
+from e_commerce.models import *
 
 # Register your models here.
 
 # NOTE: Aquí personalizamos los campos en el Django Admin.
 
-# @admin.register(Comic)
-# class ComicsAdmin(admin.ModelAdmin):
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
     # NOTE: Para seleccionar los campos en la tabla de registros
-    # list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
 
     # NOTE: Filtro lateral de elementos:
-    # list_filter= ('marvel_id','title')
+    list_filter= ('marvel_id','title')
     
     # NOTE: Buscador de elementos en la columna:
-    # search_fields = ['title']
+    search_fields = ['title']
 
     # NOTE: Para seleccionar los campos en el registro. 
     # fields = ('marvel_id', 'title', 'stock_qty')
 
     # NOTE: Genera un campo desplegable con los registros seleccionados.
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
+    
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    # NOTE: Para seleccionar los campos en la tabla de registros
+    list_display = ('user_id', 'comic_id', 'favorite', 'cart','wisher_qty','buied_qty')
+
+    # NOTE: Filtro lateral de elementos:
+    list_filter= ('comic_id','favorite')
+    
+    # NOTE: Buscador de elementos en la columna:
+    search_fields = ['comic_id']
+
+    # NOTE: Para seleccionar los campos en el registro. 
+    fields = ('user_id', 'comic_id', 'favorite')
+
+    # NOTE: Genera un campo desplegable con los registros seleccionados.
     # fieldsets = (
     #     (None, {
-    #         'fields': ('marvel_id', 'title', 'stock_qty')
+    #         'fields': ('user_id', 'comic_id', 'favorite')
     #     }),
     #     ('Advanced options', {
     #         'classes': ('collapse',),
     #         'fields': ('description','price', 'picture'),
     #     }),
     # )
-    # pass
+    pass
